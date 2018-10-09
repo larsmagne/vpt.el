ed02bb43eadc212f3effa97cd620b0fe7d6639b1
Lars Ingebrigtsen <larsi@gnus.org>
2018-10-09 15:31:13 +0200

    Scaffolding

---
+++ vpt.el
@@ -0,0 +1,25 @@
+;;; vpt.el --- Variable Pitch Tables -*- lexical-binding: t -*-
+;; Copyright (C) 2018 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: tables
+
+;; vpt is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; vpt is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl)
+
+(provide 'vpt)
+
+;;; vpt.el ends here
